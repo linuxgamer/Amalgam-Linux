0fb971926c98abe92604a6aa3c9a5f5090ef6790
Platerman <abbosasrorov2012@gmail.com>
2026-05-31 09:46:13 +0500

    Update IMatSystemSurface_OnScreenSizeChanged.cpp

--- Amalgam/src/Hooks/IMatSystemSurface_OnScreenSizeChanged.cpp
+++ Amalgam/src/Hooks/IMatSystemSurface_OnScreenSizeChanged.cpp
@@ -3,9 +3,9 @@
 #include "../Features/Visuals/Materials/Materials.h"
 
 MAKE_HOOK(IMatSystemSurface_OnScreenSizeChanged, U::Memory.GetVirtual(I::MatSystemSurface, 111), void,
-	void* rcx, int nOldWidth, int nOldHeight)
+        void* rcx, int nOldWidth, int nOldHeight)
 {
-	DEBUG_RETURN(IMatSystemSurface_OnScreenSizeChanged, rcx, nOldWidth, nOldHeight);
+        DEBUG_RETURN(IMatSystemSurface_OnScreenSizeChanged, rcx, nOldWidth, nOldHeight);
 
         CALL_ORIGINAL(rcx, nOldWidth, nOldHeight);
 
@@ -14,3 +14,4 @@ MAKE_HOOK(IMatSystemSurface_OnScreenSizeChanged, U::Memory.GetVirtual(I::MatSyst
                 H::Fonts.Reload();
                 F::Materials.ReloadMaterials();
         }
+}
